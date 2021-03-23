fdb4d37e16aae9b297f4cdce3e27a0c0072df5b8
MB-study <MB-study@192.168.1.131>
2021-03-23 19:02:44 +0800

    Java

---
+++ src/main/java/com/ocp/Day03/AccessDemo.java
@@ -0,0 +1,10 @@
+package com.ocp.Day03;
+
+import com.ocp.Day03.bar.John;
+
+public class AccessDemo {
+    public static void main(String[] args) {
+        John j=new John();
+        j.print();
+    }
+}
